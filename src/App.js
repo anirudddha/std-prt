5f56e41a126c9f57a0f0953aa4a291c07779211f
Aniruddha Pawar <aniruddha662003@gmail.com>
2023-10-13 13:17:41 +0530

    link tag and dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import Home from './components/Home';
+import About from './components/About';
+
 import {
   Route,
   Routes,
@@ -12,6 +15,14 @@ function App() {
     <div>
       <BrowserRouter>
         <Sidebar />
+        <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home/>}></Route>
+              <Route exact path="/about" element={<About />} ></Route>
+              {/* <Route exact path="/login" element={<Login showAlert={showAlert}/>} ></Route>
+              <Route exact path="/signup" element={<Signup showAlert={showAlert}/>} ></Route> */}
+            </Routes>
+          </div>
       </BrowserRouter>
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,34 +24,31 @@ const Sidebar = () => {
 
   return (
     <div className={`sidebar ${expanded ? 'expanded' : ''}`}>
-
-      <div className={`sidebar-icon ${isHomeClicked ? 'active' : ''}`} onClick={handleHomeClick}>
-        <div className="expand-button" onClick={handleExpand}>
-          {expanded ? '➖' : '➕'}
-        </div>
+      <div className="expand-button" onClick={handleExpand}>
+        {expanded ? '➖' : '➕'}
+      </div>
+      <div className={`sidebar-icon`}>
         <span role="img" aria-label="Home Icon">
-          🏠
-          {expanded && <span className="icon-name">Home</span>}
+          <i class="fa-solid fa-house" data-bs-toggle="dropdown" aria-expanded="false"></i>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item"><Link to="/">Home</Link></a></li>
+            <li><a class="dropdown-item"><Link to="/about ">about</Link></a></li>
+          </ul>
+          {expanded && <span className="icon-name" onClick={handleHomeClick}> Home</span>}
           {expanded && (
-            <div className={`dropdown-options ${isHomeClicked ? 'big':''}`}>
-              <li className="nav-item">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/about">About</Link>
-              </li>
+            <div className={`dropdown-options ${isHomeClicked ? 'big' : ''}`}>
+              <li className="nav-item"><Link to="/">Home</Link></li>
+              <li className="nav-item"><Link to="/about">About</Link></li>
             </div>
           )}
-          
         </span>
-
       </div>
-      <div className={`sidebar-icon ${isAboutClicked ? 'active' : ''}`} onClick={handleAboutClick}>
+      <div className={`sidebar-icon`} onClick={handleAboutClick}>
         <span role="img" aria-label="About Icon">
-          ℹ️
-          {expanded && <span className="icon-name">  About</span>}
+          <i class="fa-solid fa-address-card"></i>
+          {expanded && <span className="icon-name" onClick={handleHomeClick}>  About</span>}
           {expanded && (
-            <div className={`dropdown-options ${isAboutClicked ? 'big':''}`}>
+            <div className={`dropdown-options ${isAboutClicked ? 'big' : ''}`}>
               <div >Option A</div>
               <div >Option B</div>
               <div >Option C</div>
